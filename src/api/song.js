b203f981f540092da6d3eef410a258c55d5838e8
tomatogu <565237686@qq.com>
2018-03-10 16:30:14 +0800

    first commit

---
+++ src/api/song.js
@@ -0,0 +1,46 @@
+import jsonp from 'common/js/jsonp'
+import {commonParams, options} from 'api/config'
+import {getUid} from 'common/js/uid'
+import axios from 'axios'
+
+const debug = process.env.NODE_ENV !== 'production'
+
+export function getVKey(songmid, filename) {
+  const url = 'https://c.y.qq.com/base/fcgi-bin/fcg_music_express_mobile3.fcg'
+
+  const data = Object.assign({}, commonParams, {
+    hostUin: 0,
+    format: 'json',
+    platform: 'yqq',
+    needNewCode: 0,
+    cid: 205361747,
+    uin: 0,
+    songmid,
+    filename,
+    guid: getUid()
+  })
+
+  return jsonp(url, data, Object.assign({}, options, {
+    param: 'callback'
+  }))
+}
+
+export function getLyric(mid) {
+  const url = debug ? '/api/getLyric' : 'http://www.guxiaoyao.com/music/api/getLyric'
+
+  const data = Object.assign({}, commonParams, {
+    songmid: mid,
+    pcachetime: +new Date(),
+    platform: 'yqq',
+    hostUin: 0,
+    needNewCode: 0,
+    g_tk: 5381,
+    format: 'json'
+  })
+
+  return axios.get(url, {
+    params: data
+  }).then((res) => {
+    return Promise.resolve(res.data)
+  })
+}
